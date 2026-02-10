a22aab404ad16444cb67d32edfe3554df4e1102a
purrpetualentropy <flish32@yahoo.com>
2026-02-10 14:08:06 +0000

    Create misc.js

---
+++ Generators JS/Sentence Construction/Case Markers Gen/misc.js
@@ -0,0 +1,10 @@
+let themPermitted = false;
+let focPermitted = false;
+let negPermitted = false;
+let lookupWord = "";
+let lookupType = "word";
+let forcedArgsArray = [];
+let forceArgs = false;
+let globVerbType;
+
+// literally, it's just a bunch of random global variables i'm using for some features
