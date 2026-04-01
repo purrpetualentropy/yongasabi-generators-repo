b0add8aff53f974898c3e711953b53a9e89738da
purrpetualentropy <flish32@yahoo.com>
2026-04-02 00:23:58 +0100

    update

---
+++ Generators JS/Sentence Construction/Verb Tenses Gen/vowelChecker.js
@@ -0,0 +1,34 @@
+function vowelChecker (word) {
+let finalChar = word.slice(-1);
+let hFinalChar = word.slice(-2);
+console.log(`vowelChecker running with word ${word} finalChar ${finalChar} hFinalChar ${hFinalChar}`)
+switch (finalChar) {
+  case "h":
+    if (hFinalChar == "ah" || hFinalChar == "oh" || hFinalChar == "ih" || hFinalChar == "uh" || hFinalChar == "eh") {return true} else {return false}
+    break;
+
+  case "a":
+    return true;
+    break;
+
+  case "e":
+    return true;
+    break;
+
+  case "i":
+    return true;
+    break;
+
+  case "o":
+    return true;
+    break;
+
+  case "u":
+    return true;
+    break;
+
+  default:
+    return false;
+    break;
+};
+}
